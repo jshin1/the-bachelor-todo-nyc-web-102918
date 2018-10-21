4d0777553f43a9beed6cc1938798110999ca0151
Ji S <j2yshin@gmail.com>
2018-10-21 18:18:38 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,61 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  full_name = nil
+  data[season].each do |info|
+    if info["status"] == "Winner"
+      full_name = info["name"].split(' ')
+    end
+  end
+  full_name[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = nil
+  data.each do |season, season_data|
+    season_data.each do |info|
+      if info["occupation"] == occupation
+        name = info["name"]
+      end
+    end
+  end
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  people = []
+  data.each do |season, season_data|
+    season_data.each do |info|
+      if info["hometown"] == hometown
+        people << info["name"]
+      end
+    end
+  end
+  people.length
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation = []
+  data.each do |season, season_data|
+    season_data.each do |info|
+      if info["hometown"] == hometown
+        occupation << info["occupation"]
+      end
+    end
+  end
+  occupation.first
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  number_of_people = []
+  total_age = 0
+  data.each do |the_season, season_data|
+    if the_season == season
+      season_data.each do |info|
+        number_of_people << info["age"]
+        total_age += info["age"].to_f
+      end
+    end
+  end
+  (total_age / number_of_people.length).round
 end
